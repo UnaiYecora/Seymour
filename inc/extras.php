bc6823e184609ac3c283c03b9a54a1fb7fb41250
UnaiYecora <unaiyecora@gmail.com>
2018-10-01 20:45:03 +0100

    Retrasar el envío de nuevas entradas al feed (15 min)

--- inc/extras.php
+++ inc/extras.php
@@ -36,3 +36,26 @@ function complete_version_removal() {
     return '';
 }
 add_filter('the_generator', 'complete_version_removal');
+
+//////////////////////////////////////////////////////////
+// Retrasar el envío de nuevas entradas al feed (15 min)
+//////////////////////////////////////////////////////////
+function publish_later_on_feed($where) {
+    global $wpdb;
+
+    if (is_feed()) {
+        // timestamp in WP-format
+        $now = gmdate('Y-m-d H:i:s');
+
+        // value for wait; + device
+        $wait = '15'; // integer
+
+        // http://dev.mysql.com/doc/refman/5.0/en/date-and-time-functions.html#function_timestampdiff
+        $device = 'MINUTE'; // MINUTE, HOUR, DAY, WEEK, MONTH, YEAR
+
+        // add SQL-sytax to default $where
+        $where .= " AND TIMESTAMPDIFF($device, $wpdb->posts.post_date_gmt, '$now') > $wait ";
+    }
+    return $where;
+}
+add_filter('posts_where', 'publish_later_on_feed');
